82aa66abc814d595029da3895e1da8946f83bd51
JMFAMILY\connsfl <connsfl@jmfamily.com>
2022-08-31 19:53:11 -0400

    Added ngStyle

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -10,16 +10,16 @@ export class TodoItemComponent implements OnInit {
   @Input() todo:Todo = {completed : false, task:''};
   @Output() markItemAsCopleteEvent = new EventEmitter<string>();
   @Output() remvoeItemEvent = new EventEmitter<string>();
-  strikeCss:string = '';
+  
   constructor() { }
 
   ngOnInit(): void {
-    this.strikeCss =  this.todo.completed == true ? 'strike' :'';
+   
   }
 
   markAsComplete(taskName:string|undefined):any{
     this.markItemAsCopleteEvent.emit(taskName);
-    this.strikeCss =  this.todo.completed == true ? 'strike' :'';
+    
   }
   removeTask(taskName:string|undefined):any{
     this.remvoeItemEvent.emit(taskName);
